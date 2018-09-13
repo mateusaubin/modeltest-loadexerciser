30001608d944889df475a2cb23c7bf1867a49cc6
mateusaubin <mateusaubin@users.noreply.github.com>
2018-09-12 22:09:54 -0300

    send requests from file

--- all.py
+++ all.py
@@ -5,12 +5,14 @@ cli = boto3.client('sns')
 topics = cli.list_topics()
 topicarn = topics['Topics'][0]['TopicArn']
 
-message = { "path":"mestrado-dev-phyml-input://small.aP6.phy", "--run_id":"testrun", "--help":None }
+with open('small.json','r',True,'UTF-8') as fin:
+    message = fin.read()
 
+snsmessage = json.dumps({'default': message})
 
 response = cli.publish(
     TopicArn=topicarn, 
-    Message=json.dumps({'default': json.dumps(message)}),
+    Message=snsmessage,
     MessageStructure='json'
 )
 print(response)
