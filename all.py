92e9f18b9af2e685801fd624283cf03a69cef777
mateusaubin <mateusaubin@users.noreply.github.com>
2018-09-20 13:18:30 -0300

    read messages from jmodeltest log output

--- all.py
+++ all.py
@@ -1,18 +1,26 @@
 import boto3
 import json
+import string
+
+
+s3bucket = 'mestrado-dev-phyml-input'
 
 cli = boto3.client('sns')
 topics = cli.list_topics()
 topicarn = topics['Topics'][0]['TopicArn']
 
-with open('small.json','r',True,'UTF-8') as fin:
-    message = fin.read()
+with open('cmdlines.log','r',True,'UTF-8') as cmds:
+    for line in cmds:
+        path, *args = line.rstrip('\n').split(' -',maxsplit=2)[1:]
+        message = { "path": "{}:/{}".format(s3bucket, path[2:]), "cmd": '-'+args[0] }
+        snsmessage = json.dumps({'default': message})
 
-snsmessage = json.dumps({'default': message})
+        print(snsmessage)
 
-response = cli.publish(
-    TopicArn=topicarn, 
-    Message=snsmessage,
-    MessageStructure='json'
-)
-print(response)
+
+# response = cli.publish(
+#     TopicArn=topicarn, 
+#     Message=snsmessage,
+#     MessageStructure='json'
+# )
+# print(response)
