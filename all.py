d5f4d09ab23bca3240ff9030ce7e9461ec367c72
mateusaubin <mateusaubin@users.noreply.github.com>
2018-09-20 18:55:53 -0300

    parallelize enque

--- all.py
+++ all.py
@@ -1,29 +1,42 @@
 import boto3
 import json
 import string
+import concurrent.futures
+from datetime import datetime
 
 
 s3bucket = 'mestrado-dev-phyml-input'
+runid = datetime.now().isoformat()[0:19]
 
 cli = boto3.client('sns')
 topics = cli.list_topics()
 topicarn = topics['Topics'][0]['TopicArn']
 
-with open('cmdlines.log','r',True,'UTF-8') as cmds:
-    for line in cmds:
-        path, *args = line.rstrip('\n').split(' -',maxsplit=2)[1:]
-        message = { "path": "{}:/{}".format(s3bucket, path[2:]), "cmd": '-'+args[0] }
-        msg_obj = {'default': json.dumps(message)}
-        snsmessage = json.dumps(msg_obj)
+def sendmessages(i):
+    with open('cmdlines.log','r',1048576,'UTF-8') as cmds:
+        for line in cmds:
+            path, *args = line.rstrip('\n').split(' -',maxsplit=2)[1:]
+            message = { "path": "{}:/{}".format(s3bucket, path[2:]), "cmd": '-'+args[0] }
+            msg_obj = {'default': json.dumps(message)}
+            snsmessage = json.dumps(msg_obj)
 
-        # print(snsmessage)
+            # print(snsmessage)
 
-        response = cli.publish(
-            TopicArn=topicarn,
-            Subject='run1',
-            Message=snsmessage,
-            MessageStructure='json'
-        )
-        # print(response)
+            response = cli.publish(
+                TopicArn=topicarn,
+                Subject=runid,
+                Message=snsmessage,
+                MessageStructure='json'
+            )
+            assert(response['ResponseMetadata']['HTTPStatusCode'] == 200)
+            # print(response)
+    print(i)
 
-print('-- DONE --')
+
+with concurrent.futures.ThreadPoolExecutor() as executor:
+    for i in range(0, 5):
+        executor.submit(sendmessages, i)
+    print('submitted = ' + runid)
+
+#sendmessages()
+print('-- DONE -- ' + runid)
