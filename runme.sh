3cc3255957fb407b0053edaf329299fbb55a2464
mateusaubin <mateusaubin@users.noreply.github.com>
2018-11-20 00:45:35 -0200

    fix

--- runme.sh
+++ runme.sh
@@ -4,8 +4,12 @@ set -e # bail-out if anything goes wrong
 sudo apt update
 sudo apt install python3-pip
 
+git clone https://github.com/mateusaubin/modeltest-loadexerciser.git
+cd modeltest-loadexerciser
+
 pip3 install -r requirements.txt
 
 export AWS_DEFAULT_REGION=$(curl -m5 -sS http://169.254.169.254/latest/meta-data/placement/availability-zone | sed 's/.$//')
 
+mkdir log/
 ./all.py $1
