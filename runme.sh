0acaadfa6502b93c381345d4f607a89b0989f9ef
mateusaubin <mateusaubin@users.noreply.github.com>
2018-11-22 10:21:31 -0200

    add parameters to control execution

--- runme.sh
+++ runme.sh
@@ -2,20 +2,29 @@
 set -e # bail-out if anything goes wrong
 
 sudo apt update
-sudo apt install awscli
+
 sudo apt install python3-pip
+pip3 install -r requirements.txt
+
+sudo apt install awscli
+
 
 #git clone https://github.com/mateusaubin/modeltest-loadexerciser.git
 #cd modeltest-loadexerciser
-
-pip3 install -r requirements.txt
 
 export AWS_DEFAULT_REGION=$(curl -m5 -sS http://169.254.169.254/latest/meta-data/placement/availability-zone | sed 's/.$//')
 
 mkdir log/ | true
 
-sl=30
-for i in `seq 1 11`;
+sl=${3:-1800}
+iters=${2:-11}
+
+echo ""
+echo "SLEEP: $Sl"
+echo "ITERS: $iters"
+echo ""
+
+for i in `seq 1 $iters`;
 do
   echo "---------------------------------------------"
   echo "              step $i                        "
@@ -25,8 +34,9 @@ do
 
   echo "---------------------------------------------"
   echo "---------------------------------------------"
-  echo "           sleeping $sl"
+  echo "           sleeping $sl                [$i / $iters]"
   echo "---------------------------------------------"
   echo "---------------------------------------------"
   sleep $sl
+  
 done 
